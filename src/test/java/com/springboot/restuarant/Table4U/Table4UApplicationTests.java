965edc08e4ffb1346d16b851f7f5afb7e9e8fdac
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-02-21 22:46:57 +0530

    init commit

---
+++ src/test/java/com/springboot/restuarant/Table4U/Table4UApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.restuarant.Table4U;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Table4UApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
